182038f0f29b7ca246727940fd1b4ec5474df176
Marco Bejarano <marco.bejarano03@gmail.com>
2021-07-22 23:31:58 -0700

    installed express and mongoose. Server starts and will work on routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
