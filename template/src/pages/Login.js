6e0947807755037591b2aacbcffddd02c02ed97c
Marco Bejarano <marco.bejarano03@gmail.com>
2021-07-23 00:01:23 -0700

    created a very simple login page to set up mongodb connection

---
+++ template/src/pages/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Login(){
+    return (
+
+        <div>
+            <form>
+                <label>Username</label>
+                <input label="username"></input>
+                <label>Password</label>
+                <input></input>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
