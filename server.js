cd4c941dc2e270ef4dbb078e5f3a849861d6e957
Marco Bejarano <marco.bejarano03@gmail.com>
2021-07-23 14:05:35 -0700

    added routes to server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
+const routes = require("./routes");
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Route connection
+app.use(routes);
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/template");
