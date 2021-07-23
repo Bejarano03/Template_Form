07ab8c2a475c70cd028e7882c8d7d29465c881ba
Marco Bejarano <marco.bejarano03@gmail.com>
2021-07-23 14:20:33 -0700

    added effect and state to login page in order to save login information

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,18 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import API from "../utils/API";
 
-function Login(){
-    return (
+function Login() {
+  // Components initial state
+  const [members, setMembers] = useState([]);
+  const [formObject, setFormObject] = useState({
+    username: "",
+    password: "",
+  });
 
-        <div>
-            <form>
-                <label>Username</label>
-                <input label="username"></input>
-                <label>Password</label>
-                <input></input>
-                <button>Submit</button>
-            </form>
-        </div>
-    );
+  // Loads all members and sets them setMembers
+  useEffect(() => {
+    loadMembers();
+  }, []);
+
+  // Loads all members and sets them to members
+  function loadMembers() {
+    API.getMembers()
+      .then((res) => setMembers(res.data))
+      .catch((err) => console.log(err));
+  }
+
+  // Handling Input
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormObject({ ...formObject, [name]: value });
+  }
+
+//   Handling Submit
+function handleFormSubmit(event) {
+    event.preventDefault();
+    if (formObject.username && formObject.password) {
+      API.saveMember({
+        title: formObject.username,
+        author: formObject.password
+      })
+        .then(() => setFormObject({
+          title: "",
+          author: "",
+          synopsis: ""
+        }))
+        .then(() => loadMembers())
+        .catch(err => console.log(err));
+    }
+  };
+
+  return (
+    <div>
+      <form>
+        <label>Username</label>
+        <input label="username"></input>
+        <label>Password</label>
+        <input></input>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
